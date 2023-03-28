3fca986a7aca336689b2a9a468124848e19f0a68
diamantiszlilla <ldiamantisz@le-soft.io>
2023-03-28 11:20:35 +0200

    add swagger

--- src/main/java/com/bredex/bredex_demo/BredexDemoApplication.java
+++ src/main/java/com/bredex/bredex_demo/BredexDemoApplication.java
@@ -2,12 +2,12 @@ package com.bredex.bredex_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class BredexDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BredexDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BredexDemoApplication.class, args);
+    }
 }
