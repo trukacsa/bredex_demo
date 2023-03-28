527fea7b56aef08f7cbe3b582c6870ca94a0915f
diamantiszlilla <ldiamantisz@le-soft.io>
2023-03-28 12:26:27 +0200

    add db config class

---
+++ src/main/java/com/bredex/bredex_demo/client/configuration/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.bredex.bredex_demo.client.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
+
