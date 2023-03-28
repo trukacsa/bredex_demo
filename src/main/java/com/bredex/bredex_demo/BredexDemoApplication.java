cf1204daf51dd68f3344f522a664ea180fe01b4e
diamantiszlilla <ldiamantisz@le-soft.io>
2023-03-28 09:30:10 +0200

    init

---
+++ src/main/java/com/bredex/bredex_demo/BredexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bredex.bredex_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BredexDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BredexDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bredex/bredex_demo/BredexDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bredex.bredex_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BredexDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
