07ee0992536cd655a9d388148722426713559a28
diamantiszlilla <ldiamantisz@le-soft.io>
2023-03-28 12:36:10 +0200

    add annotaion to main

--- src/main/java/com/bredex/bredex_demo/BredexDemoApplication.java
+++ src/main/java/com/bredex/bredex_demo/BredexDemoApplication.java
@@ -2,10 +2,12 @@ package com.bredex.bredex_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableJpaRepositories
 public class BredexDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(BredexDemoApplication.class, args);
